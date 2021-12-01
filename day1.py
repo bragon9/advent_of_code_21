93c684ffc80f02b1bfb1ca51b65aa8cf57a267b3
Brandon Spagon <bspagon@iofficecorp.com>
2021-12-01 17:52:33 -0600

    Solve day 1 part 2

--- day1.py
+++ day1.py
@@ -5,14 +5,19 @@ def get_input(file_location: str):
     with open(f'{file_location}') as input_file:
         return[int(x) for x in input_file]
 
-def count_increases(nums_arr: List[int]) -> int:
-    """Takes a list of numbers and counts the times a num > prev num."""
+def count_increases(nums_arr: List[int], window_size: int = 1) -> int:
+    """Takes a list of numbers a subarray size, and returns the number of times
+    that a sliding window is larger than the previous window sum."""
+
     increases = 0
-    for i in range(1, len(nums_arr)):
-        if nums_arr[i] > nums_arr[i-1]:
+
+    # Check if the number being added to the window is greater than the number
+    # being removed from the array
+    for i in range(window_size, len(nums_arr)):
+        if nums_arr[i] > nums_arr[i-window_size]:
             increases += 1
     return increases
 
 if __name__ == '__main__':
     INPUT = get_input('./inputs/day1.txt')
-    print(count_increases(INPUT))
+    print(count_increases(INPUT, 3))
