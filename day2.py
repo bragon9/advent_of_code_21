ad819813db31eecc438260903e31ad2641e084ec
Brandon Spagon <bspagon@iofficecorp.com>
2021-12-02 10:16:23 -0600

    Solve day 2 part 2

--- day2.py
+++ day2.py
@@ -10,43 +10,42 @@ def get_input(file_location: str):
     with open(f'{file_location}') as input_file:
         return input_file.read().splitlines()
 
-def parse_input():
-    pass
-
 class Submarine:
     """Class to represent a submarine."""
 
     def __init__(self):
         self.x = 0
         self.y = 0
+        self.aim = 0
 
     def travel_route(self, route: List[str]) -> None:
         """Input is a route, outputs the final x and y coordinates."""
 
         for instruction in route:
-            direction, distance = instruction.split()
-            distance = int(distance)
+            direction, amount = instruction.split()
+            amount = int(amount)
 
             if direction == FORWARD:
-                self.forward(distance)
+                self.forward(amount)
             elif direction == UP:
-                self.up(distance)
+                self.up(amount)
             elif direction == DOWN:
-                self.down(distance)
+                self.down(amount)
 
         return self.x, self.y
 
     def forward(self, distance: int) -> None:
         self.x += distance
+        self.y += self.aim * distance
 
-    def up(self, distance: int) -> None:
-        self.y -= distance
+    def up(self, degrees: int) -> None:
+        self.aim -= degrees
 
-    def down(self, distance: int) -> None:
-        self.y += distance
+    def down(self, degrees: int) -> None:
+        self.aim += degrees
 
 if __name__ == '__main__':
     INPUT = get_input('./inputs/day2.txt')
     sub = Submarine()
     x, y = sub.travel_route(INPUT)
-    print(x * y)
+    print(f'final coordinates: ({x}, {y}). Answer={x*y}')
