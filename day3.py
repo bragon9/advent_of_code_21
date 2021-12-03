df496ddb0a355954093ef7987e30c250d0ed6142
Brandon Spagon <bspagon@iofficecorp.com>
2021-12-03 15:47:38 -0600

    solve day3 part 2

--- day3.py
+++ day3.py
@@ -1,12 +1,17 @@
 from typing import List, Tuple
 
+CO2 = 'CO2'
+OXYGEN = 'OXYGEN'
+
+FORMULAS = (CO2, OXYGEN)
+
 def get_input(file_location: str):
     """Pass in a file path and it will read lines."""
 
     with open(f'{file_location}') as input_file:
         return input_file.read().splitlines()
 
-def calculate_rates(input_arr: List[str]) -> Tuple[int, int]:
+def calculate_power_rates(input_arr: List[str]) -> Tuple[int, int]:
     """Take an input array and return the gamma (most frequent) and epsilon
     (least frequent) rate."""
 
@@ -40,10 +45,48 @@ def calculate_rates(input_arr: List[str]) -> Tuple[int, int]:
 
     return (int(gamma_rate_binary, 2), int(epsilon_rate_binary, 2))
 
+def calculate_life_support(input_arr: List[str], formula: str) -> int:
+    """Take an input array and return the rating for the requested formula."""
+
+    if formula not in FORMULAS:
+        raise ValueError('Invalid formula.')
+
+    arr = input_arr.copy()
+    value_length = len(arr[0])
+
+    for i in range(value_length):
+        ones_arr = []
+        zeroes_arr = []
+        frequency = 0
+
+        for value in arr:
+            if value[i] == '1':
+                ones_arr.append(value)
+                frequency += 1
+            else:
+                zeroes_arr.append(value)
+                frequency -= 1
+
+        # If formula is oxygen, use most common value. CO2 uses least common.
+        if frequency >= 0:
+            if formula == OXYGEN:
+                arr = ones_arr
+            else:
+                arr = zeroes_arr
+        else:
+            if formula == OXYGEN:
+                arr = zeroes_arr
+            else:
+                arr = ones_arr
+
+        if len(arr) == 1:
+            return int(arr[0], 2)
+
 if __name__ == '__main__':
     INPUT = get_input('./inputs/day3.txt')
-    gamma_rate, epsilon_rate = calculate_rates(INPUT)
+    o2_generator_rating = calculate_life_support(INPUT, OXYGEN)
+    co2_scrubber_rating = calculate_life_support(INPUT, CO2)
 
-    print(f'gamma={gamma_rate}, '
-          f'epsilon={epsilon_rate}, '
-          f'power={gamma_rate*epsilon_rate}')
+    print(f'o2={o2_generator_rating}, '
+          f'co2={co2_scrubber_rating}, '
+          f'life support={o2_generator_rating*co2_scrubber_rating}')
